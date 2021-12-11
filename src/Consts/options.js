c2201ab400ea150ee9611cd1c1f997e99d026d59
nynon <nynonmaslo@gmail.com>
2021-12-11 15:22:50 +0100

    i dont even know

--- src/Consts/options.js
+++ src/Consts/options.js
@@ -4,6 +4,30 @@ const OPTIONS = [
         values: ['gastronomiczny', 'handlowy', 'usługowy', 'biurowy', 'przemysłowy'],
     },
     { name: 'infrastuktura', values: ['parki', 'bankomaty', 'kultura', 'edukacja', 'transport'] },
+    {
+        name: 'preferencje mieszkańców',
+        values: [
+            'motoryzacja',
+            'sport',
+            'moda',
+            'finanse',
+            'polityka',
+            'zakupy online',
+            'technologie',
+            'drogerie',
+        ],
+    },
+    {
+        name: 'wydatki mieszkańców',
+        values: [
+            'żywność',
+            'alkohole',
+            'odzież',
+            'restauracje i hotele',
+            'transport',
+            'rekreakcja i kultura',
+        ],
+    },
 ];
 
 export const MAIN_OPTION_NAMES = [
