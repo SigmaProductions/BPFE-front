599f9e00065deac87349024b542bf5c04db69169
senbar <sendek.bartosz@gmail.com>
2021-12-11 18:05:23 +0100

    map working js

---
+++ src/components/DisplayLocations/Map.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react'
+import { Loader } from "@googlemaps/js-api-loader"
+import styled from 'styled-components';
+
+const MapContainer= styled.div`
+    width:100rem;
+    height:100rem;
+`
+
+export default function Map() {
+    useEffect(()=>{
+        const loader = new Loader({
+          apiKey: "",
+             version: "weekly"
+        });
+
+        loader.load().then(() => {
+          let map = new google.maps.Map(document.getElementById("map"), {
+            center: { lat: -34.397, lng: 150.644 },
+            zoom: 8,
+          });
+        });
+    },[]);
+
+    return (
+        <div>
+            <MapContainer id="map">sdfdsf</MapContainer>
+        </div>
+      )
+}
